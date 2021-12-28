c909c5780788328d6191e1e050e27e56cc6d7034
smallzhong <55563027+smallzhong@users.noreply.github.com>
2021-12-28 14:25:22 +0800

    初步封装

--- ConsoleApplication9.cpp
+++ ConsoleApplication9.cpp
@@ -28,25 +28,8 @@ void colorReduce(Mat& image)
 	}
 }
 
-int main()
+void encrypt_image(uchar* ttt)
 {
-	init_des("zyc9075 ");
-
-	Mat backImg = imread("g:/lena1.png");  //存放自己图像的路径 
-	//imshow("显示图像", backImg);
-
-	//colorReduce(backImg);
-
-	char(*origin_image_buffer)[8] = (char(*)[8])malloc(backImg.rows * backImg.cols * backImg.channels());
-	if (backImg.isContinuous())
-	{
-		memcpy(origin_image_buffer, backImg.data, backImg.rows * backImg.cols * backImg.channels());
-	}
-
-	char(*encrypt_image_buffer)[8] = (char(*)[8])malloc(backImg.rows * backImg.cols * backImg.channels());
-	memcpy((void*)encrypt_image_buffer, (void*)origin_image_buffer, backImg.rows * backImg.cols * backImg.channels());
-
-	uchar* ttt = (uchar*)encrypt_image_buffer;
 	for (int i = 0; i < 196608 / 8; i++)
 	{
 		/*	bitset<64> t;
@@ -61,12 +44,45 @@ int main()
 
 		//memcpy((void *)ttt[i * 8], &t, 8);
 	}
+}
+
+int main()
+{
+	init_des("zyc9075 ");
+
+	Mat backImg = imread("g:/lena1.png");  //存放自己图像的路径 
+	//imshow("显示图像", backImg);
+
+	//colorReduce(backImg);
+
+	// 原始图像buffer
+	char(*origin_image_buffer)[8] = (char(*)[8])malloc(backImg.rows * backImg.cols * backImg.channels());
+	if (backImg.isContinuous())
+	{
+		memcpy(origin_image_buffer, backImg.data, backImg.rows * backImg.cols * backImg.channels());
+	}
+
+	// 加密图像buffer
+	char(*encrypt_image_buffer)[8] = (char(*)[8])malloc(backImg.rows * backImg.cols * backImg.channels());
+	memcpy((void*)encrypt_image_buffer, (void*)origin_image_buffer, backImg.rows * backImg.cols * backImg.channels());
+
+	// 解密后的图像的buffer
+	char(*decrypt_image_buffer)[8] = (char(*)[8])malloc(backImg.rows * backImg.cols * backImg.channels());
+
+	// 进行图像加密
+	encrypt_image((uchar *)encrypt_image_buffer);
+
+
 
 	memcpy(backImg.data, origin_image_buffer, backImg.rows * backImg.cols * backImg.channels());
 	imshow("加密前的图片", backImg);
 
 	memcpy(backImg.data, encrypt_image_buffer, backImg.rows * backImg.cols * backImg.channels());
 	imshow("加密后的图片", backImg);
+
+	memcpy(backImg.data, decrypt_image_buffer, backImg.rows * backImg.cols * backImg.channels());
+	imshow("重新解密后的图片", backImg);
+
 	//imwrite("after_en.png", backImg);
 
 	waitKey(0);
